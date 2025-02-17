131d18e16c992cf418a45e42bf16f012743d6f6d
안장훈 <wkdgns135@gmail.com>
2025-02-17 12:51:44 +0900

    Init

---
+++ Source/Pepccine/Pepccine.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pepccine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pepccine, "Pepccine" );
+ 
---
+++ Source/Pepccine/PepccineGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PepccineGameMode.h"
+#include "PepccineCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+APepccineGameMode::APepccineGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
