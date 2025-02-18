1b6e1eb1f2b2182c035c89c3d576ec5959445052
황찬호 <cksgh0316@gmail.com>
2025-02-18 15:46:00 +0900

    Initial Commit

---
+++ Source/Pepccine/Common/BasePlayerController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pepccine/Common/BasePlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+ABasePlayerController::ABasePlayerController()
+{
+
+}
+
+void ABasePlayerController::BeginPlay()
+{
+  Super::BeginPlay();
+
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* SubSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (InputMappingContext)
+			{
+				SubSystem->AddMappingContext(InputMappingContext, 0);
+			}
+		}
+	}
+}
