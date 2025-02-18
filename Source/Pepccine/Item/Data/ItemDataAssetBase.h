eb49187eeba44e752ddcf7ade132db17c342903d
ttingpan <dx123xb@naver.com>
2025-02-18 20:42:33 +0900

    Prepare Pull

--- Source/Pepccine/Item/Data/ItemDataAssetBase.h
+++ Source/Pepccine/Item/Data/ItemDataAssetBase.h
@@ -9,11 +9,11 @@ struct FItemDataInfo
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info");
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info")
 	FString DisplayName;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info", meta = (MultiLine = "true"));
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info", meta = (MultiLine = "true"))
 	FString Description;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info");
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info")
 	int32 SellingPrice;
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Info")
 	TObjectPtr<UStaticMesh> SpawnMesh;
@@ -24,13 +24,22 @@ struct FWeaponItemData
 {
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon");
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon")
 	FItemDataInfo Info;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon")
 	TObjectPtr<USkeletalMesh> EquippedMesh;
 
-	// TODO[명관] : 무기 스텟 추가
+	// TODO[명관] : 무기 스텟 업데이트
+
+	// 임시 스텟
+
+	// 공격력 배율
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Stats", meta = (DisplayName = "공격력 배율"));
+	float AttackMultiplier = 1.0f;
+	// 최대 탄창 용량
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Weapon|Stats", meta = (DisplayName = "최대 탄창 용량"));
+	int32 MagazineCapacity = 30;
 };
 
 USTRUCT(BlueprintType)
