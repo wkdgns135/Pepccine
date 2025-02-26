b7a2fec863c36d5d887aa0355cb48beb5d0cc362
황찬호 <cksgh0316@gmail.com>
2025-02-26 19:06:31 +0900

    [Merge] To merge

---
+++ Source/Pepccine/Character/Widget/WeaponWidget.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WeaponWidget.generated.h"
+
+class UTextBlock;
+class UImage;
+
+UCLASS()
+class PEPCCINE_API UWeaponWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* WeaponText;
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* WeaponAmmo;
+	UPROPERTY(meta = (BindWidget))
+	UImage* FrontWeaponImage;
+	UPROPERTY(meta = (BindWidget))
+	UImage* BackWeaponImage;
+
+	UFUNCTION(BlueprintCallable, Category = "UI|Weapon")
+	void UpdateWeaponUI(UTexture2D* MainWeaponImage, UTexture2D* SubWeaponImage, const FString& WeaponName, const int32 Ammo, const int32 MaxAmmo);
+};
