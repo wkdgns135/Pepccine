46ea70483d25995504c15958c3fae57a933daa1f
Grabbit1222 <133847810+Grabbit1222@users.noreply.github.com>
2025-03-07 00:52:10 +0900

    [Fix] ZombiePrisoner Not Moving

--- Source/Pepccine/Monster/AI/Controller/NormalMonsterAIC.h
+++ Source/Pepccine/Monster/AI/Controller/NormalMonsterAIC.h
@@ -21,7 +21,8 @@ public:
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
 	UBlackboardComponent* BlackboardComponent;
-
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI")
+	UBehaviorTree* BehaviorTree;
 
 private:
 	void InitializeBehaviorTree(APawn* InPawn);
